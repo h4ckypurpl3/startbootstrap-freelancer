6e5832d346e4a5d9bb789a44501463da4ba73a86
h4ckypurpl3 <h4ckypurpl3@gmail.com>
2020-10-04 20:16:24 -0500

    adding CV using play around with boostrap 4 flex utilities

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -69,16 +69,26 @@
     $('#portfolio').magnificPopup({
         delegate: 'a',
         type: 'image',
+        closeOnContentClick: false,
+		    closeBtnInside: false,
+		    mainClass: 'mfp-with-zoom mfp-img-mobile',
         tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1]
-        },
         image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
     });
 
 })(jQuery); // End of use strict
